dd259b19cab67dbb1e97dd648141a3a8de7e7d6b
Len Lanphar <lanphar@gmail.com>
2021-01-18 20:32:09 -0500

    fixed variable name typo

--- apps/mralarm/widget.js
+++ apps/mralarm/widget.js
@@ -17,7 +17,7 @@
     // get a decimal representation of current time
     // e.g. 12:30 = 12.5, 7:12 = 7.2
     function getDecimalTime(t) {
-	return t.getHours()+(time.getMinutes()/60)
+	return t.getHours()+(t.getMinutes()/60)
     }
 
     // get time delta between two decimal time values, in ms
@@ -102,78 +102,3 @@
     
 
 })()
-
-/*
-
-
-
-// Chances are boot0.js got run already and scheduled *another*
-// 'load(alarm.js)' - so let's remove it first!
-clearInterval();
-
-function formatTime(t) {
-  var hrs = 0|t;
-  var mins = Math.round((t-hrs)*60);
-  return hrs+":"+("0"+mins).substr(-2);
-}
-
-function getCurrentHr() {
-  var time = new Date();
-  return time.getHours()+(time.getMinutes()/60)+(time.getSeconds()/3600);
-}
-
-function showAlarm(alarm) {
-  var msg = formatTime(alarm.hr);
-  var buzzCount = 10;
-  if (alarm.msg)
-    msg += "\n"+alarm.msg;
-  E.showPrompt(msg,{
-    title:"ALARM!",
-    buttons : {"Sleep":true,"Ok":false} // default is sleep so it'll come back in 10 mins
-  }).then(function(sleep) {
-    buzzCount = 0;
-    if (sleep) {
-      if(alarm.ohr===undefined) alarm.ohr = alarm.hr;
-      alarm.hr += 10/60; // 10 minutes
-    } else {
-      alarm.last = (new Date()).getDate();
-      if (alarm.ohr!==undefined) {
-          alarm.hr = alarm.ohr;
-          delete alarm.ohr;
-      }
-      if (!alarm.rp) alarm.on = false;
-    }
-    require("Storage").write("alarm.json",JSON.stringify(alarms));
-    load();
-  });
-  function buzz() {
-    Bangle.buzz(100).then(()=>{
-      setTimeout(()=>{
-        Bangle.buzz(100).then(function() {
-          if (buzzCount--)
-            setTimeout(buzz, 3000);
-          else if(alarm.as) { // auto-snooze
-            buzzCount = 10;
-            setTimeout(buzz, 600000);
-          }
-        });
-      },100);
-    });
-  }
-  buzz();
-}
-
-// Check for alarms
-var day = (new Date()).getDate();
-var hr = getCurrentHr()+10000; // get current time - 10s in future to ensure we alarm if we've started the app a tad early
-var alarms = require("Storage").readJSON("alarm.json",1)||[];
-var active = alarms.filter(a=>a.on&&(a.hr<hr)&&(a.last!=day));
-if (active.length) {
-  // if there's an alarm, show it
-  active = active.sort((a,b)=>a.hr-b.hr);
-  showAlarm(active[0]);
-} else {
-  // otherwise just go back to default app
-  setTimeout(load, 100);
-}
-*/

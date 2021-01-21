fd402d5eb56373bca9df4b7d8b02afd611dfce5b
Len Lanphar <lanphar@gmail.com>
2021-01-21 11:15:21 -0500

    tighted js indentation, fixed undeclared variable in buzz()

--- apps/mralarm/app.js
+++ apps/mralarm/app.js
@@ -24,128 +24,128 @@ function getCurrentHr() {
 }
 
 function showMainMenu() {
-    const menu = {
-	'': { 'title': 'Alarms' },
-	'New Alarm': ()=>editAlarm(-1)
+  const menu = {
+    '': { 'title': 'Alarms' },
+    'New Alarm': ()=>editAlarm(-1)
+  };
+  alarms.forEach((alarm,idx)=>{
+    txt = (alarm.on?"on  ":"off ")+formatTime(alarm.hr);
+    if (alarm.rp) txt += " (repeat)";
+    menu[txt] = function() {
+      editAlarm(idx);
     };
-    alarms.forEach((alarm,idx)=>{
-	txt = (alarm.on?"on  ":"off ")+formatTime(alarm.hr);
-	if (alarm.rp) txt += " (repeat)";
-	menu[txt] = function() {
-	    editAlarm(idx);
-	};
-    });
-    menu['< Back'] =  ()=>{load();};
-    return E.showMenu(menu);
+  });
+  menu['< Back'] =  ()=>{load();};
+  return E.showMenu(menu);
 }
 
 function editAlarm(alarmIndex) {
-    var newAlarm = alarmIndex<0;
-    var hrs = 12;
-    var mins = 0;
-    var en = true;
-    var repeat = true;
-    var as = false;
-    var days = [true,true,true,true,true,true,true]; // 0-6 sun-sat
-
-    if (!newAlarm) {
-	var a = alarms[alarmIndex];
-	hrs = 0|a.hr;
-	mins = Math.round((a.hr-hrs)*60);
-	en = a.on;
-	repeat = a.rp;
-	as = a.as;
-	days = a.days;
+  var newAlarm = alarmIndex<0;
+  var hrs = 12;
+  var mins = 0;
+  var en = true;
+  var repeat = true;
+  var as = false;
+  var days = [true,true,true,true,true,true,true]; // 0-6 sun-sat
+  
+  if (!newAlarm) {
+    var a = alarms[alarmIndex];
+    hrs = 0|a.hr;
+    mins = Math.round((a.hr-hrs)*60);
+    en = a.on;
+    repeat = a.rp;
+    as = a.as;
+    days = a.days;
+  }
+  const menu = {
+    '': { 'title': 'Alarms' },
+    'Hours': {
+      value: hrs,
+      onchange: function(v){if (v<0)v=23;if (v>23)v=0;hrs=v;this.value=v;} // no arrow fn -> preserve 'this'
+    },
+    'Minutes': {
+      value: mins,
+      onchange: function(v){if (v<0)v=59;if (v>59)v=0;mins=v;this.value=v;} // no arrow fn -> preserve 'this'
+    },
+    'Enabled': {
+      value: en,
+      format: v=>v?"On":"Off",
+      onchange: v=>en=v
+    },
+    'Repeat': {
+      value: en,
+      format: v=>v?"Yes":"No",
+      onchange: v=>repeat=v
+    },
+    'Sunday': {
+      value: days[0],
+      format: v=>v?"On":"Off",
+      onchange: v=>days[0]=v
+    },
+    'Monday': {
+      value: days[1],
+      format: v=>v?"On":"Off",
+      onchange: v=>days[1]=v
+    },
+    'Tueday': {
+      value: days[2],
+      format: v=>v?"On":"Off",
+      onchange: v=>days[2]=v
+    },
+    'Wednesday': {
+      value: days[3],
+      format: v=>v?"On":"Off",
+      onchange: v=>days[3]=v
+    },
+    'Thursday': {
+      value: days[4],
+      format: v=>v?"On":"Off",
+      onchange: v=>days[4]=v
+    },
+    'Friday': {
+      value: days[5],
+      format: v=>v?"On":"Off",
+      onchange: v=>days[5]=v
+    },
+    'Saturday': {
+      value: days[6],
+      format: v=>v?"On":"Off",
+      onchange: v=>days[6]=v
+    },
+    'Auto snooze': {
+      value: as,
+      format: v=>v?"Yes":"No",
+      onchange: v=>as=v
     }
-    const menu = {
-	'': { 'title': 'Alarms' },
-	'Hours': {
-	    value: hrs,
-	    onchange: function(v){if (v<0)v=23;if (v>23)v=0;hrs=v;this.value=v;} // no arrow fn -> preserve 'this'
-	},
-	'Minutes': {
-	    value: mins,
-	    onchange: function(v){if (v<0)v=59;if (v>59)v=0;mins=v;this.value=v;} // no arrow fn -> preserve 'this'
-	},
-	'Enabled': {
-	    value: en,
-	    format: v=>v?"On":"Off",
-	    onchange: v=>en=v
-	},
-	'Repeat': {
-	    value: en,
-	    format: v=>v?"Yes":"No",
-	    onchange: v=>repeat=v
-	},
-	'Sunday': {
-	    value: days[0],
-	    format: v=>v?"On":"Off",
-	    onchange: v=>days[0]=v
-	},
-	'Monday': {
-	    value: days[1],
-	    format: v=>v?"On":"Off",
-	    onchange: v=>days[1]=v
-	},
-	'Tueday': {
-	    value: days[2],
-	    format: v=>v?"On":"Off",
-	    onchange: v=>days[2]=v
-	},
-	'Wednesday': {
-	    value: days[3],
-	    format: v=>v?"On":"Off",
-	    onchange: v=>days[3]=v
-	},
-	'Thursday': {
-	    value: days[4],
-	    format: v=>v?"On":"Off",
-	    onchange: v=>days[4]=v
-	},
-	'Friday': {
-	    value: days[5],
-	    format: v=>v?"On":"Off",
-	    onchange: v=>days[5]=v
-	},
-	'Saturday': {
-	    value: days[6],
-	    format: v=>v?"On":"Off",
-	    onchange: v=>days[6]=v
-	},
-	'Auto snooze': {
-	    value: as,
-	    format: v=>v?"Yes":"No",
-	    onchange: v=>as=v
-	}
+  };
+  function getAlarm() {
+    var hr = hrs+(mins/60);
+    var day = 0;
+    // If alarm is for tomorrow not today (eg, in the past), set day
+    if (hr < getCurrentHr())
+      day = (new Date()).getDate();
+    // Save alarm
+    return {
+      on : en, hr : hr,
+      last : day, rp : repeat, as: as,
+      days: days
     };
-    function getAlarm() {
-	var hr = hrs+(mins/60);
-	var day = 0;
-	// If alarm is for tomorrow not today (eg, in the past), set day
-	if (hr < getCurrentHr())
-	    day = (new Date()).getDate();
-	// Save alarm
-	return {
-	    on : en, hr : hr,
-	    last : day, rp : repeat, as: as,
-	    days: days
-	};
-    }
-    menu["> Save"] = function() {
-	if (newAlarm) alarms.push(getAlarm());
-	else alarms[alarmIndex] = getAlarm();
-	require("Storage").write("mralarm.json",JSON.stringify(alarms));
-	showMainMenu();
+  }
+  menu["> Save"] = function() {
+    if (newAlarm) alarms.push(getAlarm());
+    else alarms[alarmIndex] = getAlarm();
+    require("Storage").write("mralarm.json",JSON.stringify(alarms));
+    showMainMenu();
+  };
+  if (!newAlarm) {
+    menu["> Delete"] = function() {
+      alarms.splice(alarmIndex,1);
+      require("Storage").write("mralarm.json",JSON.stringify(alarms));
+      showMainMenu();
     };
-    if (!newAlarm) {
-	menu["> Delete"] = function() {
-	    alarms.splice(alarmIndex,1);
-	    require("Storage").write("mralarm.json",JSON.stringify(alarms));
-	    showMainMenu();
-	};
-    }
-    menu['< Back'] = showMainMenu;
-    return E.showMenu(menu);
+  }
+  menu['< Back'] = showMainMenu;
+  return E.showMenu(menu);
 }
 
 showMainMenu();
--- apps/mralarm/widget.js
+++ apps/mralarm/widget.js
@@ -1,123 +1,125 @@
 (() => {
-    const s = require('Storage');
-
-    const ALARMS_FILE = "mralarm.json";
-    const ALARMS_UPDATE_TIME = 5; // time between alarms file updates (in mins)
+  const s = require('Storage');
+  
+  const ALARMS_FILE = "mralarm.json";
+  const ALARMS_UPDATE_TIME = 5; // time between alarms file updates (in mins)
+  
+  let alarms;
+  let nextAlarm;
+  
+  let alarmTimerId = null;
+  let updateTimerId = null;
+  
+  function loadAlarms() {
+    alarms = s.readJSON(ALARMS_FILE,1) || [];
+  }
+  
+  // get a decimal representation of current time
+  // e.g. 12:30 = 12.5, 7:12 = 7.2
+  function getDecimalTime(t) {
+    return t.getHours()+(t.getMinutes()/60)
+  }
+  
+  // get time delta between two decimal time values, in ms
+  function getTimeDelta(t1,t2) {
+    let delta = (t2-t1)*3600*1000;
+    return 0|delta;
+  }
+  
+  // snooze the current alarm
+  function snooze(mins) {
+    let now = new Date();
+    let nowTime = getDecimalTime(now);
+    let snoozeEnd = nowTime + mins/60; // mins minutes from now
+    let timeUntil = getTimeDelta(nowTime,snoozeEnd);
+    alarmTimerId = setTimeout(doAlarm,timeUntil);
+  }
+  
+  // set timer for the next alarm, if one is available
+  function setNextAlarm() {
+    if (alarms.length == 0) {
+      return;
+    }
+    let now = new Date();
+    let dow = now.getDay(); // local time day of week 0-6
+    let nowTime = getDecimalTime(now);
+    let alarmsLeftToday = alarms.filter(a=>a.on && a.days[dow] === true &&
+					nowTime <= a.hr);
+    if (alarmsLeftToday.length == 0) {
+      return;
+    }
+    alarmsLeftToday = alarmsLeftToday.sort((a,b)=>a.hr-b.hr);
+    nextAlarm = alarmsLeftToday[0];
+    let timeUntil = getTimeDelta(nowTime,nextAlarm.hr);
+    alarmTimerId = setTimeout(doAlarm,timeUntil);
+  }
+  
+  // update alarms from the alarms file
+  function updateAlarms() {
+    if (alarmTimerId != null) {
+      // stop the queued alarm before updating
+      clearTimeout(alarmTimerId);
+      alarmTimerId = null;
+      nextAlarm = null;
+    }
+    loadAlarms();
+    setNextAlarm();
+    updateTimerId = setTimeout(updateAlarms,ALARMS_UPDATE_TIME*60*1000);
+  }
+  
+  function doAlarm() {
+    let alarm = nextAlarm;
     
-    let alarms;
-    let nextAlarm;
-
-    let alarmTimerId = null;
-    let updateTimerId = null;
-
-    function loadAlarms() {
-	alarms = s.readJSON(ALARMS_FILE,1) || [];
-    }
-
-    // get a decimal representation of current time
-    // e.g. 12:30 = 12.5, 7:12 = 7.2
-    function getDecimalTime(t) {
-	return t.getHours()+(t.getMinutes()/60)
-    }
-
-    // get time delta between two decimal time values, in ms
-    function getTimeDelta(t1,t2) {
-	let delta = (t2-t1)*3600*1000;
-	return 0|delta;
-    }
-
-    // snooze the current alarm
-    function snooze(mins) {
-	let now = new Date();
-	let nowTime = getDecimalTime(now);
-	let snoozeEnd = nowTime + mins/60; // mins minutes from now
-	let timeUntil = getTimeDelta(nowTime,snoozeEnd);
-	alarmTimerId = setTimeout(doAlarm,timeUntil);
+    alarmTimerId = null;
+    
+    let buzzCount = 10;
+    
+    if (updateTimerId != null) {
+      // stop updates while we're alarming
+      clearTimeout(updateTimerId);
+      updateTimerId = null;
     }
     
-    // set timer for the next alarm, if one is available
-    function setNextAlarm() {
-	if (alarms.length == 0) {
-	    return;
-	}
-	let now = new Date();
-	let dow = now.getDay(); // local time day of week 0-6
-	let nowTime = getDecimalTime(now);
-	let alarmsLeftToday = alarms.filter(a=>a.on && a.days[dow] === true &&
-					    nowTime <= a.hr);
-	if (alarmsLeftToday.length == 0) {
-	    return;
-	}
-	alarmsLeftToday = alarmsLeftToday.sort((a,b)=>a.hr-b.hr);
-	nextAlarm = alarmsLeftToday[0];
-	let timeUntil = getTimeDelta(nowTime,nextAlarm.hr);
-	alarmTimerId = setTimeout(doAlarm,timeUntil);
-    }
-
-    // update alarms from the alarms file
-    function updateAlarms() {
-	if (alarmTimerId != null) {
-	    // stop the queued alarm before updating
-	    clearTimeout(alarmTimerId);
-	    alarmTimerId = null;
-	    nextAlarm = null;
-	}
-	loadAlarms();
-	setNextAlarm();
-	updateTimerId = setTimeout(updateAlarms,ALARMS_UPDATE_TIME*60*1000);
-    }
+    let msg = "Alarm";
     
-    function doAlarm() {
-	alarmTimerId = null;
-
-	let buzzCount = 10;
-	
-	if (updateTimerId != null) {
-	    // stop updates while we're alarming
-	    clearTimeout(updateTimerId);
-	    updateTimerId = null;
-	}
-
-	let msg = "Alarm";
-
-	E.showPrompt(msg,{
-	    title:"ALARM!",
-	    buttons: {"Sleep":true,"Ok":false}
-	}).then(function(sleep) {
-	    buzzCount = 0;
-	    if (sleep) {
-		snooze(10);
-	    } else {
-		// todo: handle non-repeating alarms
-		updateAlarms();
+    E.showPrompt(msg,{
+      title:"ALARM!",
+      buttons: {"Sleep":true,"Ok":false}
+    }).then(function(sleep) {
+      buzzCount = 0;
+      if (sleep) {
+	snooze(10);
+      } else {
+	// todo: handle non-repeating alarms
+	updateAlarms();
+      }
+    });
+    
+    function buzz() {
+      Bangle.buzz(100).then(()=>{
+	setTimeout(()=>{
+	  Bangle.buzz(100).then(function() {
+	    if (buzzCount--)
+	      setTimeout(buzz, 3000);
+	    else if(alarm.as) { // auto-snooze
+	      buzzCount = 10;
+	      setTimeout(buzz, 600000);
 	    }
-	});
-	
-	function buzz() {
-	    Bangle.buzz(100).then(()=>{
-		setTimeout(()=>{
-		    Bangle.buzz(100).then(function() {
-			if (buzzCount--)
-			    setTimeout(buzz, 3000);
-			else if(alarm.as) { // auto-snooze
-			    buzzCount = 10;
-			    setTimeout(buzz, 600000);
-			}
-		    });
-		},100);
-	    });
-	}
-	buzz();
-
+	  });
+	},100);
+      });
     }
-
-    updateAlarms();
-
-    // add the widget
-    WIDGETS["mralarm"]={area:"tl",width:24,draw:function() {
-	g.setColor(-1);
-	g.drawImage(atob("GBgBAAAAAAAAABgADhhwDDwwGP8YGf+YMf+MM//MM//MA//AA//AA//AA//AA//AA//AB//gD//wD//wAAAAADwAABgAAAAAAAAA"),this.x,this.y);
-    }};
+    buzz();
     
-
+  }
+  
+  updateAlarms();
+  
+  // add the widget
+  WIDGETS["mralarm"]={area:"tl",width:24,draw:function() {
+    g.setColor(-1);
+    g.drawImage(atob("GBgBAAAAAAAAABgADhhwDDwwGP8YGf+YMf+MM//MM//MA//AA//AA//AA//AA//AA//AB//gD//wD//wAAAAADwAABgAAAAAAAAA"),this.x,this.y);
+  }};
+  
+  
 })()
